fcf42f7250f4dbca12830069a1d662e1b9192c75
V1nya <kyrylopusanka@gmail.com>
2021-10-22 16:45:27 +0300

    Initial commit

---
+++ src/main/java/com/example/SuccessCadets/SuccessCadetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SuccessCadets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccessCadetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccessCadetsApplication.class, args);
+	}
+
+}
